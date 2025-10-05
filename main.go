a8ddbbc0fe3a9da45ceeaf6ff0f1bad5c68863df
MrT <65011212151@msu.ac.th>
2025-10-06 00:53:15 +0700

    core

--- main.go
+++ main.go
@@ -80,7 +80,8 @@ func main() {
 // ================== CORS Middleware ==================
 func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*") // Dev: allow all, production: restrict domain
+		// ระบุ frontend origin
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true") // ต้องมี
